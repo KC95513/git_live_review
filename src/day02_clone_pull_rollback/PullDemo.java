510e17dcae13e7046b15b0e7bf0548d9507106e0
KC95513 <kcchahdi@gmail.com>
2025-03-27 18:23:28 -0700

    Update PullDemo.java

--- src/day02_clone_pull_rollback/PullDemo.java
+++ src/day02_clone_pull_rollback/PullDemo.java
@@ -2,10 +2,16 @@ package day02_clone_pull_rollback;
 
 public class PullDemo {
     public static void main(String[] args) {
-
         System.out.println("Hello From Local");
         
-        System.out.println("Hello From Teammate who pushed to Github");
-        System.out.println("Hello from Teammate in local");
+        System.out.println("Hello From Teammate in Github");
+        
+        System.out.println("Hello from AnotherTeammate in Github");
+
+        sout.out.print("Hello again From another Teammate in  Github at line 11");
+
+        sout.out.print("Hello again From another Teammate in  Github at line 13");
+
+
     }
 }
